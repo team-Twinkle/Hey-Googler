660af7d68daf55e4de65cad7559c6331b960adde
crHwang0822 <chaa00@naver.com>
2023-02-19 15:37:32 +0900

    background.js 와 side-panel.js : 사이드바 excention handling 코드 추가

--- side-panel.js
+++ side-panel.js
@@ -1,11 +1,17 @@
 console.log("side-panel script loaded");
 
-chrome.runtime.onMessage.addListener(function(msg, sender){
+chrome.runtime.onMessage.addListener((msg, sender,sendResponse)=>{
     if(msg == "toggle"){
         console.log("message received");
         toggle();
     }
-})
+    else if(msg=="message for getting search term from background.js"){
+        console.log("message received and send data");
+        let search_term = document .querySelectorAll('input.gLFyf')[0].value;
+        console.log("we will send "+ search_term); //검색 키워드
+        sendResponse(search_term);
+    }
+});
 
 var iframe = document.createElement('iframe'); 
 iframe.style.background = "White";
