7e6e82c8597b27024895058e887c1b7f5960cb11
crHwang0822 <chaa00@naver.com>
2023-03-05 18:15:15 +0900

    디렉토리 내 파일 이름 변경

--- content-script.js
+++ content-script.js
@@ -23,7 +23,7 @@ iframe.style.left = "0px";
 iframe.style.zIndex = "9000000000000000000";
 iframe.style.border = "0px"; 
 iframe.style.opacity = 0.8; 
-iframe.src = chrome.runtime.getURL("popup.html")
+iframe.src = chrome.runtime.getURL("list.html")
 
 document.body.appendChild(iframe);
 
