005c9646db183b072ec83a4d46ab23a2d961e5fc
HyegyoJung <junghk0115@gmail.com>
2023-03-06 17:12:01 +0900

    TEST

--- background.js
+++ background.js
@@ -1,85 +1,93 @@
 //아이콘 클릭했을 때 이벤트
-{chrome.action.onClicked.addListener(tab => {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs=> {
-    currentTab = tabs[0];
-    currentURL=currentTab.url;
-    let URLstart=currentURL.substr(0,9);
-    //console.log(URLstart);
-    //console.log(currentURL);
-    if(URLstart=='chrome://'){ // URL이 'chrome://' 로 시작한다면 새탭 생성
-      chrome.tabs.create({
-        url: "newTab.html",
-        active:true
-      });
-    } 
-    else{ // 아니라면 사이드바 생성
-      chrome.tabs.sendMessage(tab.id,"toggle");
-      console.log('message sent');
-    }
- });
-});}
-
+{
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      currentTab = tabs[0];
+      currentURL = currentTab.url;
+      let URLstart = currentURL.substr(0, 9);
+      //console.log(URLstart);
+      //console.log(currentURL);
+      if (URLstart == "chrome://") {
+        // URL이 'chrome://' 로 시작한다면 새탭 생성
+        chrome.tabs.create({
+          url: "newTab.html",
+          active: true,
+        });
+      } else {
+        // 아니라면 사이드바 생성
+        chrome.tabs.sendMessage(tab.id, "toggle");
+        console.log("message sent");
+      }
+    });
+  });
+}
 
 //현재 탭과 이전 탭 저장
 var beforeTab;
 var currentTab;
-chrome.tabs.onActivated.addListener(activeInfo=>{
+chrome.tabs.onActivated.addListener((activeInfo) => {
   beforeTab = currentTab;
-  console.log("activated changing")
-  chrome.tabs.onUpdated.addListener((currentTabId,changingInfo,tabs)=>{
+  console.log("activated changing");
+  chrome.tabs.onUpdated.addListener((currentTabId, changingInfo, tabs) => {
     //console.log(currentTabId);
-    chrome.tabs.get(currentTabId,Tab=>{
-      currentTab=Tab.url;
-    })
-    console.log('        b      ' + beforeTab);
-    console.log('   c    '+currentTab);
-  })
+    chrome.tabs.get(currentTabId, (Tab) => {
+      currentTab = Tab.url;
+    });
+    console.log("        b      " + beforeTab);
+    console.log("   c    " + currentTab);
+  });
 });
-
-
 
 let visitedSites = [];
 let searchKeywords = [];
 
+import "./popup.js";
 
 //방문 기록 리스트 추가 및 콘솔 출력
-chrome.history.onVisited.addListener((historyItem) => { //url을 새로 방문할 때마다 
-  visitedSites.push({url: historyItem.url, title: historyItem.title});   //url 과 title 을 확인하고
+chrome.history.onVisited.addListener((historyItem) => {
+  //url을 새로 방문할 때마다
+  visitedSites.push({ url: historyItem.url, title: historyItem.title }); //url 과 title 을 확인하고
   console.log("Visited Site:", historyItem.url, historyItem.title);
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo) =>{   //referrer 를 확인한다 ! 
-    if (changeInfo.status === 'complete'){
-      chrome.tabs.sendMessage(tabId,"referrer",response=>{
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    //referrer 를 확인한다 !
+    if (changeInfo.status === "complete") {
+      chrome.tabs.sendMessage(tabId, "referrer", (response) => {
         var referrer = response; //referrer 얻은 데이터 !!
-        console.log("===========================>>>>>"+referrer);
-        
-        if(referrer=='https://www.google.com/'){//1차링크 추적 
-            //1차링크의 검색어 추적
-            const url = new URL(beforeTab);
-            keyword1 = url.searchParams.get("q"); //1차링크의 검색어 
-            console.log(keyword1);
-            // 이 if 문 안에서 keyword 랑 url 이랑 title 을 모두 접근 가능하다.
-        }   
+        console.log("===========================>>>>>" + referrer);
+
+        if (referrer == "https://www.google.com/") {
+          //1차링크 추적
+          //1차링크의 검색어 추적
+          const url = new URL(beforeTab);
+          keyword1 = url.searchParams.get("q"); //1차링크의 검색어
+          console.log(keyword1);
+          // 이 if 문 안에서 keyword 랑 url 이랑 title 을 모두 접근 가능하다.
+          var data = {
+            title: historyItem.title,
+            link: historyItem.url,
+            memo: " ",
+            keword: keyword1,
+            dir_id: "5",
+          };
+          writeIndexedDB(data);
+        }
       });
     }
   });
-  
-  
 });
-
 
 //새 탭 기록 리스트 추가 및 콘솔 출력
 chrome.tabs.onCreated.addListener((tab) => {
-  visitedSites.push({url: tab.url, title: tab.title});
+  visitedSites.push({ url: tab.url, title: tab.title });
   console.log("Created Tab:", tab.url, tab.title);
 });
 
 //새 창 기록 리스트 추가 및 콘솔 출력
 chrome.windows.onCreated.addListener((window) => {
-  visitedSites.push({url: window.tabs[0].url, title: window.tabs[0].title});
+  visitedSites.push({ url: window.tabs[0].url, title: window.tabs[0].title });
   console.log("Created Window:", window.tabs[0].url, window.tabs[0].title);
 });
-
 
 //구글 검색어 리스트 추가 및 콘솔 출력
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
--- popup.js
+++ popup.js
@@ -1,42 +1,3 @@
-//전역 변수
-var running = false;
-
-// 버튼/이미지 클릭 이벤트
-
-//키워드 토글
-function keywordToggleClick() {
-  //var toggle = document.getElementsByClassName("toggle_keyword");
-  //class로 통채로 받아..? 그럼 전부 지정인가??
-}
-
-// start,stop버튼 누르면 동작되는 자세한 코드들은 background.js 인가?
-//그 떄는 html 함수 여러개 걸기
-//처음 디폴트는 정지도 눌려있는 상태여야 하는 건가?
-//마우스 오버시 마우스 모양 바뀌는 것도 필요한가?
-//start & stop button
-function runningButtonClick(button) {
-  var start = document.getElementById("button-start");
-  var stop = document.getElementById("button-stop");
-
-  if (running == false && button == "start") {
-    start.src = "./images/icon_start_true.svg";
-    stop.src = "./images/icon_stop.svg";
-    running = true;
-  } else if (running == true && button == "stop") {
-    stop.src = "./images/icon_stop_true.svg";
-    start.src = "./images/icon_start.svg";
-    running = false;
-  }
-}
-
-//dir추가 버튼
-//디자인이나 방식 피드백
-//prompt창은 디자인이 안 예뻐 .. 제이쿼리
-//input방식?
-//click -> text edit 창 -> ok -> add dir
-//dir이름 수정하기 기능은??
-function addDirButtonClick() {}
-
 var db;
 var request = window.indexedDB.open("HeyGoogler", 1);
 
@@ -76,3 +37,40 @@ request.onerror = function (event) {
 request.onsuccess = function (event) {
   db = request.result;
 };
+
+function writeIndexedDB(data) {
+  var request = window.indexedDB.open("HeyGoogler");
+
+  request.onerror = function (event) {
+    console.log("error");
+  };
+
+  request.onsuccess = function (event) {
+    var db = this.result;
+    var tx = db.transaction(["linkStore"], "readwrite");
+
+    tx.oncomplete = function (event) {
+      console.log("done");
+    };
+
+    tx.onerror = function (event) {
+      console.log("fail");
+    };
+
+    var listStore = tx.objectStore("listStore");
+    var request = objectStore.add(data);
+    request.onsuccess = function (event) {
+      console.log(event.target.result);
+    };
+  };
+}
+
+var test_data = {
+  title: "title",
+  link: "link 예시",
+  memo: " ",
+  keyword: "키워드",
+  dir_id: "5",
+};
+
+writeIndexedDB(test_data);
