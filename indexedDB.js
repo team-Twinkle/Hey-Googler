3f8761731c6a3aa604d536c7119d8ed178ea667f
HyegyoJung <junghk0115@gmail.com>
2023-03-27 19:22:07 +0900

    Update indexedDB.js

--- indexedDB.js
+++ indexedDB.js
@@ -1,7 +1,6 @@
 var db;
-const request = window.indexedDB.open("HeyGoogler", 5);
-// request.onerror=(e)=>alert('failed');
-// request.onsuccess =(e)=> db = request.result;
+const request = window.indexedDB.open("HeyGoogler", 1);
+
 request.onerror = function (event) {
   console.log("failed");
 };
@@ -14,66 +13,109 @@ request.onsuccess = function (event) {
 request.onupgradeneeded = function (event) {
   db = event.target.result;
 
-  db.createObjectStore("url", {
+  var urlStore = db.createObjectStore("urlStore", {
     keyPath: "id",
     autoIncrement: true
   });
 
+  urlStore.createIndex("url", "url", { unique: false });
+  urlStore.createIndex("title", "title", { unique: false });
+  urlStore.createIndex("memo", "memo", { unique: false });
+  urlStore.createIndex("keyword", "keyword", { unique: false });
+  urlStore.createIndex("dir_id", "dir_id", { unique: false });
+
+  var keywordStore = db.createObjectStore("keywordStore", {
+    keyPath: "k_id",
+    autoIncrement: true
+  });
+  keywordStore.createIndex("keyword", "keyword", { unique: false });
+  keywordStore.createIndex("dir_id", "dir_id", { unique: false });
+
+  var dirStore = db.createObjectStore("dirStore", {
+    keyPath: "d_id",
+    autoIncrement: true
+  });
+  dirStore.createIndex("dir_id", "dir_id", { unique: false });
+  dirStore.createIndex("dir_name", "dir_name", { unique: false });
+  
   request.onerror = function (event) {
     console.log("failed");
   };
   request.onsuccess = function (event) {
     db = request.result;
   };
-
-  // request.onsuccess=(e)=> db = request.result;
-
-  // objectStore.createIndex("title", "title", { unique: false });
-  // objectStore.createIndex("url", "url", { unique: false });
-  // objectStore.createIndex("memo", "memo", { unique: false });
-  // objectStore.createIndex("keyword", "keyword", { unique: false });
-  // objectStore.createIndex("dir_id", "dir_id", { unique: false });
-
-  // var keywordStore = db.createObjectStore("keywordStore", {
-  //   keyPath: "k_id",
-  //   autoIncrement: true
-  // });
-  // keywordStore.createIndex("keyword", "keyword", { unique: false });
-  // keywordStore.createIndex("dir_id", "dir_id", { unique: false });
-
-  // var dirStore = db.createObjectStore("dirStore", {
-  //   keyPath: "d_id",
-  //   autoIncrement: true
-  // });
-  // dirStore.createIndex("dir_id", "dir_id", { unique: false });
-  // dirStore.createIndex("dir_name", "dir_name", { unique: false });
 };
 
-function writeUrlDB(data) {
-  var request = window.indexedDB.open('HeyGoogler');
+// function writeUrlDB(datas) {
+//   const request = window.indexedDB.open('HeyGoogler');
+
+//   request.onerror = function(event){
+//     console.log('DB error', event.target.errorCode);
+//   };
+
+//   request.onsuccess  = function(event){
+//     const db = request.result;
+//     const transaction = db.transaction(['urlStore'], 'readwrite');
+
+//     transaction.oncomplete = function(event){
+//       console.log ('성공');
+//     };
+//     transaction.onerror = function(event){
+//       console.log('실패');
+//     };
+
+//     const urlStore = transaction.objectStore('urlStore');
+//     for (const data of datas){
+//       const request = urlStore.add(data);
+//       request.onsuccess = function(event){
+//         console.log(event.target.result);
+//       };
+//     }
+
+//   };
+// }
+
+
+function writeDB(datas, store_name) {
+  const request = window.indexedDB.open('HeyGoogler');
+
   request.onerror = function(event){
     console.log('DB error', event.target.errorCode);
-  }
+  };
 
   request.onsuccess  = function(event){
-    var request = window.indexedDB.open('HeyGoogler');
-    const db = target.result;
-    const transaction = db.transaction(['url'], 'readwrite');
+    const db = request.result;
+    const transaction = db.transaction([store_name], 'readwrite');
 
     transaction.oncomplete = function(event){
       console.log ('성공');
-    }
+    };
     transaction.onerror = function(event){
       console.log('실패');
+    };
+
+    const objectStore = transaction.objectStore(store_name);
+    for (const data of datas){
+      const request = objectStore.add(data);
+      request.onsuccess = function(event){
+        console.log(event.target.result);
+      };
     }
 
-    var objectStore = transaction.objectStore('url');
-    request = objectStore.add(data);
-    request.onsuccess = function(event){
-      console.log(event.target.result);
-    }
-  }
+  };
 }
+
+
+// const data =  {url : "wwww.ddd", title: "구글페이지", memo : "이건중요", keyword:"구굴", dir_id : 1};
+  
+
+const datas = [
+  {url : "wwww.dddddfffffdd", title: "구글페이지", memo : "이건중요", keyword:"구굴", dir_id : 1}
+];
+
+writeDB(datas, 'urlStore');
+
+// writeUrlDB(data);
 
 // const data = {
 //     title: "title",
@@ -83,14 +125,13 @@ function writeUrlDB(data) {
 //     dir_id: "5",
 //   };
 
-const data = {
-  id : 1,
-  url : 'www.dddd'
-};
-  writeUrlDB(data);
+// const data = [{
+//   url : 'www.dddd'
+// }];
+  // writeUrlDB(data);
 
-// objectStore.transaction.oncomplete = function(event) {
-//     var store = db.transaction("linkStore", "readwrite").objectStore("linkStore");
+// urlStore.transaction.oncomplete = function(event) {
+//     var store = db.transaction("linkStore", "readwrite").urlStore("linkStore");
 //     store.add(test_data);
 // }
 
@@ -104,7 +145,7 @@ const data = {
 
 //   request.onsuccess = function (event) {
 //     var db = this.result;
-//     var tx = db.transaction("objectStore", "readwrite");
+//     var tx = db.transaction("urlStore", "readwrite");
 
 //     tx.oncomplete = function (event) {
 //       console.log("done");
@@ -114,8 +155,8 @@ const data = {
 //       console.log("fail");
 //     };
 
-//     var listStore = tx.objectStore("objectStore");
-//     var request = objectStore.add(data);
+//     var listStore = tx.urlStore("urlStore");
+//     var request = urlStore.add(data);
 //     request.onsuccess = function (event) {
 //       console.log(event.target.result);
 //     };
