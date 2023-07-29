f1a91099f20543f2d510ef59eba38f44f4c3f277
crHwang0822 <chaa00@naver.com>
2023-07-30 00:43:21 +0900

    fix : list.js 버그수정

--- list.js
+++ list.js
@@ -177,6 +177,7 @@ var open_Obs = 'urlStore';
 
 // 데이터를 화면에 출력하는 함수
 function displayData(data) {
+
   //컨텐츠 들어갈 위치
   var container = document.getElementById('dataContainer');
   container.innerHTML = ''; 
@@ -186,7 +187,7 @@ function displayData(data) {
     var k = JSON.stringify(data[i].keyword);
     var t = JSON.stringify(data[i].title);
     var p = JSON.stringify(data[i].url);
-<<<<<<< HEAD
+
     console.log(p);
 
     var template = document.getElementById("keyword_template");
@@ -199,18 +200,7 @@ function displayData(data) {
     path_clone.querySelector(".path-box").querySelector(".title").innerHTML = t;
     path_clone.querySelector(".path-box").querySelector(".path").innerHTML = p;
   
-=======
 
-    var template = document.getElementById("keyword_template");
-    var path_template = document.querySelector(".path_template");
-    var clone = template.content.cloneNode(true);
-    var path_clone = path_template.cloneNode(true);
-/*
-    clone.querySelector(".keyword").innerHTML = k;
-    path_clone.querySelector(".title").innerHTML = t;
-    path_clone.querySelector(".path").innerHTML = p;
-    */
->>>>>>> 6de4d7582a1a68efa7c174823bf5fd8137eaac77
     container.appendChild(clone);
     container.appendChild(path_clone);
 
@@ -256,5 +246,3 @@ function readDB() {
 
 // readDB() 함수 호출
 readDB();
-
-
