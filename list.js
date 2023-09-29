a6eb2816b895c3a7b07563d0320bfc8887da765e
idon1nab <99960721+idon1nab@users.noreply.github.com>
2023-09-29 20:54:31 +0900

    fix: editBtn, editmodal 부분 수정 중(초록박스 중복 저장, 수정 제대로 안됨 오류)

--- list.js
+++ list.js
@@ -166,8 +166,8 @@ function initListPage() {
   // currentDirLink = url;
   nowDirId = dirId;
 
-  console.log("initListPage");
-  console.log(dirName, dirId);
+  //console.log("initListPage");
+  //console.log(dirName, dirId);
   // console.log(currentDirLink, currenDirId);
 
   //NULL체크는 필수! -> 왜냐하면 dirlist.html에서는 dirName이 null이기 때문에
@@ -510,6 +510,7 @@ function displayURL(data) {
     editSaveBtn.addEventListener("click", e=> {
       let inputTitle = document.getElementById("titleInput");
       let userInputTitle = inputTitle.value;
+      console.log(editFlag);
       editDB("urlStore", "title", parseInt(editFlag), userInputTitle);
       editmodal.style.display = "none"
     })
